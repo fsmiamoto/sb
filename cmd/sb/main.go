8b62be64be3bb0220ea2f9e9a58fbfefec732ee5
Flavio Miyamoto <hello@dorayaki.co>
2026-03-08 19:46:58 +0900

    Add minimal urfave CLI entrypoint

--- cmd/sb/main.go
+++ cmd/sb/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	cli "github.com/urfave/cli/v2"
+)
+
+var version = "dev"
+
+func main() {
+	app := &cli.App{
+		Name:    "sb",
+		Usage:   "Docker sandbox tool for coding agents",
+		Version: version,
+		Action: func(ctx *cli.Context) error {
+			_, err := fmt.Fprintln(ctx.App.Writer, ctx.App.Version)
+			return err
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
